2f6bd7f5c75b570696eac7b3eeb76e3ea2361bdf
scossar <scossar3@gmail.com>
2016-01-06 14:23:44 -0800

    initial commit

---
+++ assets/javascripts/whitelist-tags.js
@@ -0,0 +1,15 @@
+(function () {
+  Discourse.Markdown.whiteListTag('div', 'class', 'grid-row');
+  Discourse.Markdown.whiteListTag('div', 'class', 'half');
+  Discourse.Markdown.whiteListTag('div', 'class', 'one-third');
+  Discourse.Markdown.whiteListTag('div', 'class', 'two-thirds');
+  Discourse.Markdown.whiteListTag('div', 'class', 'one-quarter');
+  Discourse.Markdown.whiteListTag('div', 'class', 'three-quarters');
+
+  Discourse.Markdown.whiteListTag('figure', 'class', 'grid-row');
+  Discourse.Markdown.whiteListTag('figure', 'class', 'half');
+  Discourse.Markdown.whiteListTag('figure', 'class', 'one-third');
+  Discourse.Markdown.whiteListTag('figure', 'class', 'two-thirds');
+  Discourse.Markdown.whiteListTag('figure', 'class', 'one-quarter');
+  Discourse.Markdown.whiteListTag('figure', 'class', 'three-quarters');
+})();
---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# name: discourse-grid
+# about: A simple grid system to be used in Discourse posts
+# version: 0.1
+# authors: scossar
+# url:
+
+enabled_site_setting :grid_enabled
+register_asset 'javascripts/whitelist-tags.js', :server_side
+register_asset 'stylesheets/grid.scss'
